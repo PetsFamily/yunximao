e59169a396f97d154c5abeb09a8417f6cb70b95a
zx8988185 <zx8988185@126.com>
2018-12-20 16:38:41 +0800

    调整位置

--- code/qqyunximao/src/main/java/com/petsfamily/qqyunximao/Application.java
+++ code/qqyunximao/src/main/java/com/petsfamily/qqyunximao/Application.java
@@ -1,4 +1,4 @@
-package com.petsfamily.qqyunximao.user;
+package com.petsfamily.qqyunximao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
